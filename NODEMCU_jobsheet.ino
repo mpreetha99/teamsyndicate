57468948bdda60b92e738aba7b95790ad2a88574
mpreetha99 <43870680+mpreetha99@users.noreply.github.com>
2019-03-12 10:02:14 +0530

    Add files via upload

---
+++ NODEMCU_jobsheet.ino
@@ -0,0 +1,105 @@
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "Realme1";
+const char* password = "12345678";
+const char* mqtt_server = " 192.168.43.50";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+  
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+   
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+ // pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void publishSerialData(char *serialData)
+{
+  if (!client.connected()) 
+  {
+    reconnect();
+  }
+  client.publish("f", serialData);
+ // Serial.print(serialData);
+  }
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+if (Serial.available() > 0) 
+   {
+     char a[101];
+     memset(a,0, 101);
+     Serial.readBytesUntil( '\n',a,100);
+     publishSerialData(a);
+   }
+
+  }
